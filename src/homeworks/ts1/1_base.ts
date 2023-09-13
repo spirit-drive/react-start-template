3223e8bd071f53faf2eb46c8c5248bf045e5e708
Сергей Шеховцов <sshehovtcov@gmail.com>
2023-09-13 10:07:21 +0500

    Домашняя работа №1. Задание №1. В процессе выполнения

--- src/homeworks/ts1/1_base.ts
+++ src/homeworks/ts1/1_base.ts
@@ -1,24 +1,25 @@
 /**
  * Нужно превратить файл в ts и указать типы аргументов и типы возвращаемого значения
- *    */
-export const removePlus = (string) => string.replace(/^\+/, '');
-
-export const addPlus = (string) => `+${string}`;
-
-export const removeFirstZeros = (value) => value.replace(/^(-)?[0]+(-?\d+.*)$/, '$1$2');
-
-export const getBeautifulNumber = (value, separator = ' ') =>
+ * */
+export const removePlus = (string: string): string => string.replace(/^\+/, '');
+console.log(removePlus('+Сергей'));
+export const addPlus = (string: string): string => `+${string}`;
+console.log(addPlus('Аида'));
+export const removeFirstZeros = (value: string): string => value.replace(/^(-)?[0]+(-?\d+.*)$/, '$1$2');
+console.log(removeFirstZeros('000111222'));
+export const getBeautifulNumber = (value: number, separator = ' '): string =>
   value?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, separator);
-
-export const round = (value, accuracy = 2) => {
+console.log(getBeautifulNumber(1234567891));
+export const round = (value: number, accuracy = 2): number => {
   const d = 10 ** accuracy;
   return Math.round(value * d) / d;
 };
+console.log(round(5.3));
 
 const transformRegexp =
   /(matrix\(-?\d+(\.\d+)?, -?\d+(\.\d+)?, -?\d+(\.\d+)?, -?\d+(\.\d+)?, )(-?\d+(\.\d+)?), (-?\d+(\.\d+)?)\)/;
 
-export const getTransformFromCss = (transformCssString) => {
+export const getTransformFromCss = (transformCssString: string): object => {
   const data = transformCssString.match(transformRegexp);
   if (!data) return { x: 0, y: 0 };
   return {
@@ -26,21 +27,24 @@ export const getTransformFromCss = (transformCssString) => {
     y: parseInt(data[8], 10),
   };
 };
-
-export const getColorContrastValue = ([red, green, blue]) =>
+console.log(getTransformFromCss('aa;skdfja;sdfj;'));
+export const getColorContrastValue = ([red, green, blue]: number[]): number =>
   // http://www.w3.org/TR/AERT#color-contrast
   Math.round((red * 299 + green * 587 + blue * 114) / 1000);
+console.log(getColorContrastValue([11, 12, 14]));
 
-export const getContrastType = (contrastValue) => (contrastValue > 125 ? 'black' : 'white');
+export const getContrastType = (contrastValue: number): string => (contrastValue > 125 ? 'black' : 'white');
+console.log(getContrastType(100));
 
 export const shortColorRegExp = /^#[0-9a-f]{3}$/i;
 export const longColorRegExp = /^#[0-9a-f]{6}$/i;
 
-export const checkColor = (color) => {
+export const checkColor = (color: string): void => {
   if (!longColorRegExp.test(color) && !shortColorRegExp.test(color)) throw new Error(`invalid hex color: ${color}`);
 };
+console.log(checkColor('#ADFF2F'));
 
-export const hex2rgb = (color) => {
+export const hex2rgb = (color: string): number[] => {
   checkColor(color);
   if (shortColorRegExp.test(color)) {
     const red = parseInt(color.substring(1, 2), 16);
@@ -53,8 +57,9 @@ export const hex2rgb = (color) => {
   const blue = parseInt(color.substring(5, 8), 16);
   return [red, green, blue];
 };
+console.log(hex2rgb('#ADFF2F'));
 
-export const getNumberedArray = (arr) => arr.map((value, number) => ({ value, number }));
+/*export const getNumberedArray = (arr) => arr.map((value, number) => ({ value, number }));
 export const toStringArray = (arr) => arr.map(({ value, number }) => `${value}_${number}`);
 
 export const transformCustomers = (customers) => {
@@ -62,4 +67,4 @@ export const transformCustomers = (customers) => {
     acc[customer.id] = { name: customer.name, age: customer.age, isSubscribed: customer.isSubscribed };
     return acc;
   }, {});
-};
+};*/
