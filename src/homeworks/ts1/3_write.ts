4c9d5c8cec9b52ee434e874e7bda64f73dfbdb29
Margo <plokhova.margarita@yandex.ru>
2023-09-16 22:55:56 +0300

    finished ts1 homework

--- src/homeworks/ts1/3_write.ts
+++ src/homeworks/ts1/3_write.ts
@@ -11,6 +11,16 @@
  * - id (строка)
  * - name (строка)
  * - photo (строка, необязательно)
+ * */
+
+interface Category {
+  id: string;
+  name: string;
+  photo?: string;
+}
+
+/**
+ *
  *
  * Продукт (Product) содержит
  * - id (строка)
@@ -21,9 +31,26 @@
  * - oldPrice (число, необязательно)
  * - price (число)
  * - category (Категория)
- *
+ * */
+
+interface Product {
+  id: string;
+  name: string;
+  photo: string;
+  desc?: string;
+  createdAt: string;
+  oldPrice?: number;
+  price: number;
+  category: Category;
+}
+
+/**
  * Операция (Operation) может быть либо тратой (Cost), либо доходом (Profit)
- *
+ * */
+
+type Operation = Cost | Profit;
+
+/**
  * Трата (Cost) содержит
  * - id (строка)
  * - name (строка)
@@ -32,7 +59,19 @@
  * - amount (число)
  * - category (Категория)
  * - type ('Cost')
- *
+ * */
+
+interface Cost {
+  id: string;
+  name: string;
+  desc?: string;
+  createdAt: string;
+  amount: number;
+  category: Category;
+  type: 'Cost';
+}
+
+/**
  * Доход (Profit) содержит
  * - id (строка)
  * - name (строка)
@@ -43,14 +82,125 @@
  * - type ('Profit')
  * */
 
+interface Profit {
+  id: string;
+  name: string;
+  desc?: string;
+  createdAt: string;
+  amount: number;
+  category: Category;
+  type: 'Profit';
+}
+
+// массивы с данными для обеих функций:
+const productNames = [
+  'Starfield',
+  'The Sims 4',
+  'FIFA23',
+  'Mortal Combat',
+  'Sonic',
+  'Fortnite',
+  'Spider-Man',
+  'The Last of us',
+  'God of war',
+  'Demon Slayer',
+];
+const productCategory = [
+  'Action',
+  'Adventure',
+  'Detective',
+  'Fantasy',
+  'Horror',
+  'MMORPG',
+  'RPG',
+  'Shooter',
+  'Strategy',
+  'Visual novel',
+];
+
+const productPrices = [3500, 2100, 1500, 4300, 4900];
+
+const costsNames = [
+  'rent',
+  'transportation',
+  'food',
+  'cell phone',
+  'travel',
+  'pet food',
+  'clothing',
+  'health insurance',
+  'entertainment',
+  'emergency fund',
+];
+
+const profitsNames = ['rental', 'salary', 'investment', 'bank deposit', 'cashback'];
+
+// вспомогательные функции (получение рандомного числа, равного индексу массива, и получение рандомного id):
+function createRndmNum(array: string[] | number[]): number {
+  return Math.floor(Math.random() * array.length);
+}
+
+function createRndmID(): string {
+  return (Math.random() * 10000).toString(16);
+}
 /**
  * Создает случайный продукт (Product).
  * Принимает дату создания (строка)
  * */
-// export const createRandomProduct = (createdAt: string) => {};
+
+export const createRandomProduct = (createdAt: string): Product => {
+  const productName = productNames[createRndmNum(productNames)];
+  const photoName = `${productName.toLowerCase().replace(/ /g, '-')}.jpg`;
+
+  return {
+    id: createRndmID(),
+    name: productName,
+    photo: photoName,
+    desc: `this is product called ${productName}`,
+    createdAt,
+    oldPrice: productPrices[createRndmNum(productPrices)] - 500,
+    price: productPrices[createRndmNum(productPrices)],
+    category: {
+      id: createRndmID(),
+      name: productCategory[createRndmNum(productCategory)],
+    },
+  };
+};
 
 /**
  * Создает случайную операцию (Operation).
  * Принимает дату создания (строка)
  * */
-// export const createRandomOperation = (createdAt: string) => {};
+
+export const createRandomOperation = (createdAt: string): Operation => {
+  const rndmNum = Math.floor(1 + Math.random() * 2);
+  if (rndmNum === 1) {
+    const costName = costsNames[createRndmNum(costsNames)];
+    return {
+      id: createRndmID(),
+      name: costName,
+      desc: `this is cost called ${costName}`,
+      createdAt,
+      amount: Math.floor(Math.random() * 4000) + 1,
+      category: {
+        id: createRndmID(),
+        name: 'Finances',
+      },
+      type: 'Cost',
+    } as Cost;
+  } else {
+    const profitName = profitsNames[createRndmNum(profitsNames)];
+    return {
+      id: createRndmID(),
+      name: profitName,
+      desc: `this is profit called ${profitName}`,
+      createdAt,
+      amount: Math.floor(Math.random() * 4000) + 1,
+      category: {
+        id: createRndmID(),
+        name: 'Finances',
+      },
+      type: 'Profit',
+    } as Profit;
+  }
+};
