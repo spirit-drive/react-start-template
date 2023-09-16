048da472af691bdb6e4653ceda5626444f6df83e
Татьяна <tokio9507@mail.ru>
2023-09-16 21:15:43 +0300

    homework1: homework 1 done

--- src/homeworks/ts1/1_base.ts
+++ src/homeworks/ts1/1_base.ts
@@ -1,16 +1,16 @@
 /**
  * Нужно превратить файл в ts и указать типы аргументов и типы возвращаемого значения
  * */
-export const removePlus = (string) => string.replace(/^\+/, '');
+export const removePlus = (string: string): string => string.replace(/^\+/, '');
 
-export const addPlus = (string) => `+${string}`;
+export const addPlus = (string: string): string => `+${string}`;
 
-export const removeFirstZeros = (value) => value.replace(/^(-)?[0]+(-?\d+.*)$/, '$1$2');
+export const removeFirstZeros = (value: string): string => value.replace(/^(-)?[0]+(-?\d+.*)$/, '$1$2');
 
-export const getBeautifulNumber = (value, separator = ' ') =>
+export const getBeautifulNumber = (value: number, separator = ' '): string =>
   value?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, separator);
 
-export const round = (value, accuracy = 2) => {
+export const round = (value: number, accuracy = 2): number => {
   const d = 10 ** accuracy;
   return Math.round(value * d) / d;
 };
@@ -18,8 +18,8 @@ export const round = (value, accuracy = 2) => {
 const transformRegexp =
   /(matrix\(-?\d+(\.\d+)?, -?\d+(\.\d+)?, -?\d+(\.\d+)?, -?\d+(\.\d+)?, )(-?\d+(\.\d+)?), (-?\d+(\.\d+)?)\)/;
 
-export const getTransformFromCss = (transformCssString) => {
-  const data = transformCssString.match(transformRegexp);
+export const getTransformFromCss = (transformCssString: string): { x: number; y: number } => {
+  const data: string[] = transformCssString.match(transformRegexp);
   if (!data) return { x: 0, y: 0 };
   return {
     x: parseInt(data[6], 10),
@@ -27,20 +27,20 @@ export const getTransformFromCss = (transformCssString) => {
   };
 };
 
-export const getColorContrastValue = ([red, green, blue]) =>
+export const getColorContrastValue = ([red, green, blue]: number[]): number =>
   // http://www.w3.org/TR/AERT#color-contrast
   Math.round((red * 299 + green * 587 + blue * 114) / 1000);
 
-export const getContrastType = (contrastValue) => (contrastValue > 125 ? 'black' : 'white');
+export const getContrastType = (contrastValue: number): string => (contrastValue > 125 ? 'black' : 'white');
 
 export const shortColorRegExp = /^#[0-9a-f]{3}$/i;
 export const longColorRegExp = /^#[0-9a-f]{6}$/i;
 
-export const checkColor = (color) => {
+export const checkColor = (color: string): void => {
   if (!longColorRegExp.test(color) && !shortColorRegExp.test(color)) throw new Error(`invalid hex color: ${color}`);
 };
 
-export const hex2rgb = (color) => {
+export const hex2rgb = (color: string): number[] => {
   checkColor(color);
   if (shortColorRegExp.test(color)) {
     const red = parseInt(color.substring(1, 2), 16);
@@ -54,11 +54,23 @@ export const hex2rgb = (color) => {
   return [red, green, blue];
 };
 
-export const getNumberedArray = (arr) => arr.map((value, number) => ({ value, number }));
-export const toStringArray = (arr) => arr.map(({ value, number }) => `${value}_${number}`);
+export const getNumberedArray = (arr: number[]): { value: number; number: number }[] =>
+  arr.map((value, number) => ({
+    value,
+    number,
+  }));
+export const toStringArray = (arr: { value: number; number: number }[]): string[] =>
+  arr.map(({ value, number }) => `${value}_${number}`);
 
-export const transformCustomers = (customers) => {
-  return customers.reduce((acc, customer) => {
+interface Customer {
+  id?: string;
+  name: string;
+  age: number;
+  isSubscribed: boolean;
+}
+
+export const transformCustomers = (customers: Customer[]) => {
+  return customers.reduce((acc: Record<string, Customer>, customer: Customer) => {
     acc[customer.id] = { name: customer.name, age: customer.age, isSubscribed: customer.isSubscribed };
     return acc;
   }, {});
--- src/homeworks/ts1/3_write.ts
+++ src/homeworks/ts1/3_write.ts
@@ -4,23 +4,60 @@
  * Поэтому в идеале чтобы функции возвращали случайные данные, но в то же время не абракадабру.
  * В целом сделайте так, как вам будет удобно.
  * */
+import { fa, faker } from '@faker-js/faker';
+import { v4 as uuidv4 } from 'uuid';
+
+// eslint-disable-next-line import/no-unresolved
+
+export type Category = {
+  id: string;
+  name: string;
+  photo?: string;
+};
+
+export class Product {
+  id: string;
+  name: string;
+  photo: string;
+  desc?: string;
+  createdAt: string;
+  price: number;
+  category: Category;
+
+  constructor(name: string, photo: string, createdAt: string, price: number, category: Category, desc?: string) {
+    this.id = faker.string.uuid();
+    this.name = name;
+    this.photo = photo;
+    this.desc = desc;
+    this.price = price;
+    this.category = category;
+    this.createdAt = createdAt;
+  }
+}
+
+interface Cost {
+  id: string;
+  name: string;
+  desc?: string;
+  createdAt: string;
+  amount: number;
+  category: Category;
+  type: 'Cost';
+}
+
+interface Profit {
+  id: string;
+  name: string;
+  desc?: string;
+  createdAt: string;
+  amount: number;
+  category: Category;
+  type: 'Profit';
+}
+
+export type Operation = Cost | Profit;
 
 /**
- * Нужно создать тип Category, он будет использоваться ниже.
- * Категория содержит
- * - id (строка)
- * - name (строка)
- * - photo (строка, необязательно)
- *
- * Продукт (Product) содержит
- * - id (строка)
- * - name (строка)
- * - photo (строка)
- * - desc (строка, необязательно)
- * - createdAt (строка)
- * - oldPrice (число, необязательно)
- * - price (число)
- * - category (Категория)
  *
  * Операция (Operation) может быть либо тратой (Cost), либо доходом (Profit)
  *
@@ -47,10 +84,42 @@
  * Создает случайный продукт (Product).
  * Принимает дату создания (строка)
  * */
-// export const createRandomProduct = (createdAt: string) => {};
+export const createRandomProduct = (createdAt: string): Product => {
+  return new Product(
+    'testProduct ' + uuidv4(),
+    faker.image.avatar(),
+    createdAt,
+    faker.number.int({ min: 1000, max: 10000 }),
+    { id: uuidv4(), name: 'testCategory' + uuidv4() } as Category,
+    faker.lorem.text()
+  );
+};
 
 /**
  * Создает случайную операцию (Operation).
  * Принимает дату создания (строка)
  * */
-// export const createRandomOperation = (createdAt: string) => {};
+export const createRandomOperation = (createdAt: string): Operation => {
+  const operationCost: Cost = {
+    id: uuidv4(),
+    amount: 2,
+    category: { id: uuidv4(), name: 'testCategory' + uuidv4() } as Category,
+    createdAt: createdAt,
+    name: 'Покупка продукта ' + uuidv4(),
+    type: 'Cost',
+    desc: 'Описание покупки',
+  };
+
+  const operationProfit: Profit = {
+    id: uuidv4(),
+    amount: 2,
+    category: { id: uuidv4(), name: 'testCategory' + uuidv4() } as Category,
+    createdAt: createdAt,
+    name: 'Пришла аренда за машино-место ' + uuidv4(),
+    type: 'Profit',
+    desc: 'Описание дохода',
+  };
+
+  const operations = [operationCost, operationProfit];
+  return operations[Math.floor(Math.random() * operations.length)];
+};
