efecdeb7d55dc1ecf5090e9f7c9873a11d272f1c
rostixman <rostixman@gmail.com>
2023-09-19 02:26:52 +0500

    Finish homework1

--- src/homeworks/ts1/3_write.ts
+++ src/homeworks/ts1/3_write.ts
@@ -43,14 +43,79 @@
  * - type ('Profit')
  * */
 
+export type TCategory = {
+  id: string;
+  name: string;
+  photo?: string;
+};
+
+export type TProduct = {
+  id: string;
+  name: string;
+  photo: string;
+  desc?: string;
+  createdAt: string;
+  oldPrice?: number;
+  price: number;
+  category: TCategory;
+};
+
+enum TOperationType {
+  PROFIT = 'Profit',
+  COST = 'Cost',
+}
+
+type TCommon = {
+  id: string;
+  name: string;
+  desc?: string;
+  createdAt: string;
+  amount: number;
+  category: TCategory;
+};
+
+export type TCost = TCommon & {
+  type: TOperationType.COST;
+};
+
+export type TProfit = TCommon & {
+  type: TOperationType.PROFIT;
+};
+export type TOperation = TCost | TProfit;
+
 /**
  * Создает случайный продукт (Product).
  * Принимает дату создания (строка)
  * */
-// export const createRandomProduct = (createdAt: string) => {};
+export const createRandomProduct = (createdAt: string): TProduct => ({
+  createdAt,
+  id: 'id',
+  name: 'name',
+  photo: 'photo',
+  desc: 'desc',
+  oldPrice: 321,
+  price: 123,
+  category: {
+    id: 'id',
+    name: 'name',
+    photo: 'photo',
+  },
+});
 
 /**
  * Создает случайную операцию (Operation).
  * Принимает дату создания (строка)
  * */
-// export const createRandomOperation = (createdAt: string) => {};
+export const createRandomOperation = (createdAt: string): TOperation => ({
+  createdAt,
+  id: 'id',
+  name: 'name',
+  desc: 'desc',
+  amount: 123,
+  category: {
+    id: 'id',
+    name: 'name',
+    photo: 'photo',
+  },
+  type: TOperationType.PROFIT,
+});
