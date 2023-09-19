dadf432511c07ab6b249a7c75c0f91630abad3b1
Сергей Шеховцов <sshehovtcov@gmail.com>
2023-09-19 23:50:23 +0500

    Домашняя работа №1. Задание №3. В процессе выполнения

--- src/homeworks/ts1/3_write.ts
+++ src/homeworks/ts1/3_write.ts
@@ -11,8 +11,14 @@
  * - id (строка)
  * - name (строка)
  * - photo (строка, необязательно)
- *
- * Продукт (Product) содержит
+ */
+type Category = {
+  id: string;
+  name: string;
+  photo?: string;
+};
+
+/* Продукт (Product) содержит
  * - id (строка)
  * - name (строка)
  * - photo (строка)
@@ -21,8 +27,19 @@
  * - oldPrice (число, необязательно)
  * - price (число)
  * - category (Категория)
- *
- * Операция (Operation) может быть либо тратой (Cost), либо доходом (Profit)
+ */
+type Product = {
+  id: string;
+  name: string;
+  photo: string;
+  desc?: string;
+  createdAt: string;
+  oldPrice?: number;
+  price: number;
+  category: Category;
+};
+
+/* Операция (Operation) может быть либо тратой (Cost), либо доходом (Profit)
  *
  * Трата (Cost) содержит
  * - id (строка)
@@ -32,8 +49,18 @@
  * - amount (число)
  * - category (Категория)
  * - type ('Cost')
- *
- * Доход (Profit) содержит
+ */
+type Cost = {
+  id: string;
+  name: string;
+  desc?: string;
+  createdAt: string;
+  amount: number;
+  category: Category;
+  type: Cost;
+};
+
+/* Доход (Profit) содержит
  * - id (строка)
  * - name (строка)
  * - desc (строка, необязательно)
@@ -42,12 +69,66 @@
  * - category (Категория)
  * - type ('Profit')
  * */
+type Profit = {
+  id: string;
+  name: string;
+  desc?: string;
+  createdAt: string;
+  amount: number;
+  category: Category;
+  type: Profit;
+};
 
 /**
  * Создает случайный продукт (Product).
  * Принимает дату создания (строка)
  * */
-// export const createRandomProduct = (createdAt: string) => {};
+export const createRandomProduct = (createdAt: string): Product => {
+  const categories: Category[] = [
+    { id: '1', name: 'Автомобили', photo: './photo/categories/cars.png' },
+    { id: '2', name: 'Мотоциклы', photo: './photo/categories/Motorcycles.png' },
+    { id: '3', name: 'Велосипеды', photo: './photo/categories/Bicycles.png' },
+  ];
+
+  const products: Product[] = [
+    {
+      id: '1',
+      name: 'BMW',
+      photo: './photo/products/bmw.png',
+      desc: 'Автомобиль BMW',
+      createdAt: createdAt,
+      oldPrice: 8500000,
+      price: 9580000,
+      category: categories[0],
+    },
+    {
+      id: '2',
+      name: 'Harley-Davidson',
+      photo: './photo/products/harley-devidson.png',
+      desc: 'Мотоцикл Harley-Davidson',
+      createdAt: createdAt,
+      oldPrice: 2970000,
+      price: 3100000,
+      category: categories[1],
+    },
+    {
+      id: '3',
+      name: 'Pinarello',
+      photo: './photo/products/pinarello.png',
+      desc: 'Шоссейный велосипед PINARELLO',
+      createdAt: createdAt,
+      oldPrice: 175000,
+      price: 190000,
+      category: categories[2],
+    },
+  ];
+
+  return products[Math.floor(Math.random() * products.length)];
+};
+
+for (let i = 0; i < 10; i++) {
+  console.log(createRandomProduct(String(Date.now())));
+}
 
 /**
  * Создает случайную операцию (Operation).
