12069505bb87eb94ee6dd2092a4d7581ec2bde76
Darya <d.nalivayko@paycos.tech>
2023-10-10 21:10:42 +0300

    fix: add scss to storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,6 +4,7 @@ const config = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/preset-scss",
   ],
   framework: {
     name: "@storybook/react-webpack5",
