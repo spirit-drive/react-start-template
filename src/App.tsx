a8c811071af85f9466c57d36ea61ffaf38c31dbe
Fast <krinickij.v.a@gmail.com>
2023-12-02 14:37:01 +0300

    initial lorem commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Текст писать тут
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium quod error dolor impedit possimus, molestiae reiciendis voluptas ducimus vero ullam doloribus ratione modi maxime harum minima voluptatum fuga cumque cupiditate!
         </p>
       </header>
     </div>
