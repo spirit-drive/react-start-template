798cd0f79de422d08d5a2161df1b18a9012e4c35
Владислав Ермаченков <vladislavermacenkov@MacBook-Pro-Vladislav.local>
2023-12-03 12:12:50 +0300

    second

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Научиться разрабатывать современные, реактивные, а главное – масштабируемые веб-приложение</p>
+<p>12345</p>
       </header>
     </div>
   );
