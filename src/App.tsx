8b18458288c8919d56a3ef90273270314ae1a70b
Andrey Polyakov <rztitan@ya.ru>
2023-12-03 23:06:08 +0300

    - Add about me

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Текст писать тут
-        </p>
+        <img src={logo} className="App-logo" alt="logo"/>
+          <p>Цель: систематизировать уже имеющиеся знания и получить новые.</p>
+          <p>Чем хочу овладеть: <s>миром</s> актуальными навыками по front-end</p>
+          <p>Чем владею: HTML, CSS(SASS, LESS), JS(JQuery, React), Siebel CRM</p>
+          <p>О себе: Поляков Андрей, 25.02.1994г.р., увлекаюсь автодиагностикой, Subaru. Работаю в Ak Bars Digital.</p>
       </header>
     </div>
   );
