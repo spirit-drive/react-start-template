496e2f23dee89770622b19c65e519fd411aea856
Konstantin.Talalaev <konstan.talalaev@x5.ru>
2023-12-04 20:54:51 +0300

    1

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Текст писать тут
+          1234567890
         </p>
       </header>
     </div>
