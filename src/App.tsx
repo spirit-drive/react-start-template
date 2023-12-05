6ef2a1ac9329a48868f9847a45b10765ee70ee6e
AntonPractice <npr.haustov@gmail.com>
2023-12-05 22:18:59 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,23 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Текст писать тут
-        </p>
+        <h3>О себе:</h3>
+        <div>Семь лет являюсь разработчиком на базе Oracle Siebel CRM на различных проектах</div>
+        <div>Последний год активно занимаемся разработкой фронтовой части на React, но это первый проект на фронт разработке</div>
+
+        <h3>Цели:</h3>
+		    <div>Научится писать тесты Jest</div>
+		    <div>Изучить typescript</div>
+		    <div>Углубиться в React</div>
+
+		    <h3>С чем знаком по фронтовой части:<h3>
+		    <div>JS,CSS,HTML,git,SOAP UI,gitLab,Oracle PL/SQL</div>
+
+		    <h3>Какие технологии интересны:<h3>
+		    <div>ReactJS</div>
+			  <div>Typescript</div>
+			  <div>Redux + Redux-Saga</div>
+			  <div>Jest</div>
       </header>
     </div>
   );
