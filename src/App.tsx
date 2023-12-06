6221bab76ce1840489be502e3d910a23e8fb0ba3
Oskar <oskar.stud@gmail.com>
2023-12-06 11:00:29 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,22 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Текст писать тут
-        </p>
+        <div className="container">
+          <h4>Кто?</h4>
+          <p>Шайхутдинов Оскар. ПАО Сбер.</p>
+        </div>
+        <div className="container">
+          <h4>Знания?</h4>
+          <p>HTML/CSS · React · Redux · Jest</p>
+        </div>
+        <div className="container">
+          <h4>Цель?</h4>
+          <p>Структурировать знания. Восполнить пробелы в React и Redux. Узнать Websocket, GraphQL.</p>
+        </div>
+        <div className="container">
+          <h4>Зачем?</h4>
+          <p>Использовать знания в проектах.</p>
+        </div>
       </header>
     </div>
   );
