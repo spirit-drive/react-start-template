31451e355c864fc0ffe8576c3842c5d03e50fd26
Home <1984milu@mail.ru>
2023-12-06 20:45:19 +0300

    add gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Текст писать тут
-        </p>
+        <h3>Ожидания от курса React.js Developer:</h3>
+        <ul className='list'>
+          <li>эффективно использовать React;</li>
+          <li>научиться работать с Redux, Redux-Saga, Redux-thunk;</li>
+          <li>приобрести навыки по созданию SPA-приложения и оптимизировать их для production;</li>
+          <li>закрепить навыки по TypeScript.</li>
+        </ul>
+        <h4>О себе:</h4>
+        <p>Михаил. Работал с Java, SQL, JavaScript, CSS, HTML, с React не знаком.</p>
+        <p>Цель: приобрести навыки, которые позволят перенести продукт на React с использованием Material-UI.</p>
       </header>
     </div>
   );
