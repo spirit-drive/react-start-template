246ff849aeddb61a90b4a8d792d1cbe2b9ebcc46
Maria Korol <mariakorol@Marias-MacBook-Air.local>
2023-12-09 17:04:02 +0300

    feat: homework#1

--- src/homeworks/ts1/3_write.ts
+++ src/homeworks/ts1/3_write.ts
@@ -43,14 +43,84 @@
  * - type ('Profit')
  * */
 
+export type Operation = Cost | Profit;
+
+export type Category = {
+  id: string;
+  name: string;
+  photo?: string;
+};
+
+export type Product = {
+  id: string;
+  name: string;
+  photo: string;
+  desc?: string;
+  createdAt: string;
+  oldPrice?: number;
+  price: number;
+  category: Category;
+};
+
+export type Cost = {
+  id: string;
+  name: string;
+  desc?: string;
+  createdAt: string;
+  amount: number;
+  category: Category;
+  type: 'Cost';
+};
+
+export type Profit = {
+  id: string;
+  name: string;
+  desc?: string;
+  createdAt: string;
+  amount: number;
+  category: Category;
+  type: 'Profit';
+};
+
 /**
  * Создает случайный продукт (Product).
  * Принимает дату создания (строка)
  * */
-// export const createRandomProduct = (createdAt: string) => {};
+export const createRandomProduct = (createdAt: string): Product => {
+  const category = {
+    id: '1',
+    name: 'Продукты питания',
+  };
+
+  return {
+    id: '1',
+    name: 'Шоколадная паста Nestle',
+    photo: 'nestle',
+    desc: '',
+    createdAt,
+    oldPrice: 100,
+    price: 120,
+    category,
+  };
+};
 
 /**
  * Создает случайную операцию (Operation).
  * Принимает дату создания (строка)
  * */
-// export const createRandomOperation = (createdAt: string) => {};
+export const createRandomOperation = (createdAt: string): Operation => {
+  const category = {
+    id: '1',
+    name: 'Продукты питания',
+  };
+
+  return {
+    id: '1',
+    name: 'salary',
+    createdAt,
+    amount: 10,
+    desc: '',
+    category,
+    type: 'Profit',
+  };
+};
