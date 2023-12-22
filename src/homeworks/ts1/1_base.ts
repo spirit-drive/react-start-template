96603ecbd202a5cb172b74de137bd5d4e42ca905
Oskar <oskar.stud@gmail.com>
2023-12-22 11:06:27 +0300

    homework_1

---
+++ src/homeworks/ts1/1_base.ts
@@ -0,0 +1,84 @@
+/**
+ * Нужно превратить файл в ts и указать типы аргументов и типы возвращаемого значения
+ * */
+export const removePlus = (string: string): string => string.replace(/^\+/, '');
+
+export const addPlus = (string: string): string => `+${string}`;
+
+export const removeFirstZeros = (value: string): string => value.replace(/^(-)?[0]+(-?\d+.*)$/, '$1$2');
+
+export const getBeautifulNumber = (value: any, separator = ' '): string =>
+  value?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, separator);
+
+export const round = (value: number, accuracy = 2): number => {
+  const d = 10 ** accuracy;
+  return Math.round(value * d) / d;
+};
+
+const transformRegexp =
+  /(matrix\(-?\d+(\.\d+)?, -?\d+(\.\d+)?, -?\d+(\.\d+)?, -?\d+(\.\d+)?, )(-?\d+(\.\d+)?), (-?\d+(\.\d+)?)\)/;
+
+type tTransformCssString = {
+  x: number;
+  y: number;
+};
+
+export const getTransformFromCss = (transformCssString: string): tTransformCssString => {
+  const data = transformCssString.match(transformRegexp);
+  if (!data) return { x: 0, y: 0 };
+  return {
+    x: parseInt(data[6], 10),
+    y: parseInt(data[8], 10),
+  };
+};
+
+type tRGB = Array<number>;
+export const getColorContrastValue = ([red, green, blue]: tRGB): number =>
+  // http://www.w3.org/TR/AERT#color-contrast
+  Math.round((red * 299 + green * 587 + blue * 114) / 1000);
+
+type tContrast = 'black' | 'white';
+export const getContrastType = (contrastValue: number): tContrast => (contrastValue > 125 ? 'black' : 'white');
+
+export const shortColorRegExp = /^#[0-9a-f]{3}$/i;
+export const longColorRegExp = /^#[0-9a-f]{6}$/i;
+
+type tError = void | never;
+export const checkColor = (color: string): tError => {
+  if (!longColorRegExp.test(color) && !shortColorRegExp.test(color)) throw new Error(`invalid hex color: ${color}`);
+};
+
+export const hex2rgb = (color: string): tRGB => {
+  checkColor(color);
+  if (shortColorRegExp.test(color)) {
+    const red = parseInt(color.substring(1, 2), 16);
+    const green = parseInt(color.substring(2, 3), 16);
+    const blue = parseInt(color.substring(3, 4), 16);
+    return [red, green, blue];
+  }
+  const red = parseInt(color.substring(1, 3), 16);
+  const green = parseInt(color.substring(3, 5), 16);
+  const blue = parseInt(color.substring(5, 8), 16);
+  return [red, green, blue];
+};
+
+export const getNumberedArray = <T>(arr: Array<T>): { value: T; number: number }[] =>
+  arr.map((value, number) => ({ value, number }));
+export const toStringArray = <T>(arr: { value: T; number: number }[]) =>
+  arr.map(({ value, number }): string => `${value}_${number}`);
+
+interface Customer {
+  id: string;
+  name: string;
+  age: number;
+  isSubscribed: boolean;
+}
+type TransformCustomer = {
+  [key: string]: Omit<Customer, 'id'>;
+};
+export const transformCustomers = (customers: Customer[]) => {
+  return customers.reduce((acc: TransformCustomer, customer: Customer) => {
+    acc[customer.id] = { name: customer.name, age: customer.age, isSubscribed: customer.isSubscribed };
+    return acc;
+  }, {});
+};
--- src/homeworks/ts1/3_write.ts
+++ src/homeworks/ts1/3_write.ts
@@ -43,14 +43,133 @@
  * - type ('Profit')
  * */
 
+type Category = {
+  id: string;
+  name: string;
+  photo?: string;
+};
+
+interface Product {
+  id: string;
+  name: string;
+  photo: string;
+  createdAt: string;
+  price: number;
+  category: Category;
+  desc?: string;
+  oldPrice?: number;
+}
+
+const randomValue = {
+  products: {
+    nameList: [
+      'Fresh Bread',
+      'Milk Butter',
+      'Paracorde Knife',
+      'Nokia 3110',
+      'HP EliteBook 14',
+      'AudioTechnica Model 1',
+      'Watermelon baby',
+    ],
+    photoList: [
+      'breadPic.dev',
+      'https://images.unsplash.com/photo-1701964619678-36b35865e238',
+      'pictures.io/picture1.png',
+      'random.dev/waterMelon_Photo',
+      'smartphoneImage.png',
+    ],
+    price: ['1000', '900', '7000', '250', '400', '120', '10230'],
+    oldPrice: ['999', '879', '6998', '249', '119', '399', '10229'],
+    category: {
+      nameList: ['Smartphone', 'Laptop', 'Knife', 'Vegetables', 'Headphones', 'Bread', 'Butter'],
+      photoList: ['smartphone/img1', 'laptop/img1', 'knife/img1', 'vegetables/img1', 'bread/img1'],
+    },
+    desc: ['decrtiption_1', 'decrtiption_2', 'decrtiption_3', 'decrtiption_4', 'decrtiption_5'],
+  },
+  operations: {
+    name: ['Cost', 'Profit'],
+    desc: ['CostDesc_1', 'ProfitDesc_2', 'CostDesc_3', 'ProfitDesc_4'],
+    amount: ['1', '2', '3', '4', '5'],
+    category: {
+      nameList: ['NameCategoryCost_1', 'NameCategoryProfit_2', 'NameCategoryCost_3', 'NameCategoryProfit_4'],
+      photoList: ['cost_1/img1', 'profit_2/img1', 'cost_3/img1', 'profit_4/img1', 'cost_5/img1'],
+    },
+  },
+};
+
+const createRandomId = (): string => {
+  return `id_${Math.random().toString().slice(8)}`;
+};
+
+const pickRandomValue = (arr: Array<number> | Array<string>): number | string => {
+  return arr[Math.floor(Math.random() * arr.length)];
+};
+
 /**
  * Создает случайный продукт (Product).
  * Принимает дату создания (строка)
  * */
 // export const createRandomProduct = (createdAt: string) => {};
 
+export const createRandomProduct = (createdAt: string): Product => {
+  return {
+    id: createRandomId(),
+    name: pickRandomValue(randomValue.products.nameList) as string,
+    photo: pickRandomValue(randomValue.products.photoList) as string,
+    createdAt: createdAt,
+    price: pickRandomValue(randomValue.products.price) as number,
+    category: {
+      id: createRandomId(),
+      name: pickRandomValue(randomValue.products.category.nameList) as string,
+      photo: pickRandomValue(randomValue.products.category.photoList) as string,
+    },
+    desc: pickRandomValue(randomValue.products.desc) as string,
+    oldPrice: pickRandomValue(randomValue.products.oldPrice) as number,
+  };
+};
+
+type Operation = Cost | Profit;
+
+type Cost = {
+  id: string;
+  name: string;
+  desc?: string;
+  createdAt: string;
+  amount: number;
+  category: Category;
+  type: 'Cost';
+};
+
+type Profit = {
+  id: string;
+  name: string;
+  desc?: string;
+  createdAt: string;
+  amount: number;
+  category: Category;
+  type: 'Profit';
+};
+
+const selectedRandomOperation = (): 'Cost' | 'Profit' => (Math.random() < 0.5 ? 'Cost' : 'Profit');
+
 /**
  * Создает случайную операцию (Operation).
  * Принимает дату создания (строка)
  * */
 // export const createRandomOperation = (createdAt: string) => {};
+
+export const createRandomOperation = (createdAt: string): Operation => {
+  return {
+    id: createRandomId(),
+    name: pickRandomValue(randomValue.operations.name) as string,
+    desc: pickRandomValue(randomValue.operations.desc) as string,
+    createdAt: createdAt,
+    amount: pickRandomValue(randomValue.operations.amount) as number,
+    category: {
+      id: createRandomId(),
+      name: pickRandomValue(randomValue.operations.category.nameList) as string,
+      photo: pickRandomValue(randomValue.operations.category.photoList) as string,
+    },
+    type: selectedRandomOperation(),
+  };
+};
