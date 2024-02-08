67c3f5e3507b924662cba7e1dab41c73552106bb
Aleksandr <designmab@gmail.com>
2024-02-08 22:36:33 +0300

    Home work "Списки, ключи, события, порталы"

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,6 +1,7 @@
 import React, {FC} from "react";
 import cn from "clsx";
 import './Modal.scss';
+import {createPortal} from "react-dom";
 
 interface IModal {
     isOpen: boolean,
@@ -8,14 +9,14 @@ interface IModal {
     children: React.ReactNode,
 }
 
-export const Modal:FC<IModal> = ({isOpen, onClose, children}) => {
+export const Modal: FC<IModal> = ({isOpen, onClose, children}) => {
     if (!isOpen) return null;
-    const handleCloseModal = (e: React.MouseEvent):void => {
+    const handleCloseModal = (e: React.MouseEvent): void => {
         if (e.target !== e.currentTarget) return;
         onClose(false);
     }
 
-    return (
+    return createPortal(
         <div className={cn('modal')} onClick={handleCloseModal}>
             <div className={cn('modal__inner')}>
                 <div className={cn('modal__header')}>
@@ -26,5 +27,6 @@ export const Modal:FC<IModal> = ({isOpen, onClose, children}) => {
                 </div>
             </div>
         </div>
+        , document.body
     )
 }
