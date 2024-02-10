cef7c2093b10c1b024ed7503117c79fbf4bcc216
Fast <krinickij.v.a@gmail.com>
2024-02-10 13:12:47 +0300

    adited componentslis useing intersection observer to storybook

--- src/components/componentsListWithObserver/ComponentsList.stories.jsx
+++ src/components/componentsListWithObserver/ComponentsList.stories.jsx
@@ -1,12 +1,11 @@
-import {ComponentsList } from './ComponentsList'
-
+import { ComponentsList } from "./ComponentsList";
 
 export default {
-    title: "CostAccounting/componentsList-v1",
+    title: "CostAccounting/componentsList-v2",
     component: ComponentsList,
-    description: 'componentsList v1',
-
+    description: 'componentsList v2',
 }
+
 const Template = (arg) => <ComponentsList {...arg} />
 export const Default = Template.bind({})
 Default.arg = {}
