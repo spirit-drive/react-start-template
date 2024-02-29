c3c5b084b4fe82169a259a9ab2ae2745ffe64ad0
suhareevai <sukhareev@yandex.ru>
2024-02-29 21:54:36 +0300

    add info

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,21 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Текст писать тут
-        </p>
+          <b>Цели:</b>
+          <ul>
+            <li>Получить опыт разработки приложений с использованием React</li>
+            <li>Разобрать хорошие/плохие практики разработки, тестирования</li>
+          </ul>
+
+          <b>Владение технологиями:</b>
+          <ul>
+            <li>.Net Framework, .Net Core, Entity, js</li>
+          </ul>
+
+          <b>Опыт:</b>
+          <ul>
+            <li>Backend разработка, 6 лет</li>
+          </ul>
       </header>
     </div>
   );
