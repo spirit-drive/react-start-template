2389a4934de87d34abf2397a9bcdd3ccbf94053c
AleksandrAnisenko <aleksandr.anisenko@mail.ru>
2024-03-01 17:58:44 +0300

    added about me info

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h5>Цели:</h5>
         <p>
-          Текст писать тут
+          Получить знания и навыки в работе с React и дополняющими его технологиями.
+        </p>
+        <h5>Технологии, которыми владею:</h5>
+        <p>
+          JS, TS, Angular, NgRx, RxJs
+        </p>
+        <h5>О себе:</h5>
+        <p>
+          Меня зовут Александр, мне 27 лет. Решил сменить сферу деятельности, Проработал в качестве frontend разработчика около года на Angular.
+          Сейчас хочу освоить React, т.к. сегодня это более востребованная технология и выбор вакансии для React разработчиков значительно шире.
         </p>
       </header>
     </div>
