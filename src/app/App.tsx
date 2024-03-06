d6a876fb6b6d48097091ced04993323b013f082e
Диодоров Данил Леонидович <diodorov.dl@rcit.sakha.gov.ru>
2024-03-06 14:43:34 +0900

    fixed

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,9 +7,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>1. Цель стать senior js программистом.</p>
-        <p>2. Выучить React JS, Next JS</p>
-        <p>3. Node js, React JS (средний уровень), postgres</p>
+        <p>1. Цель углубить знания в React JS, особенно интересуют тесты, оптимизация проекта, архитектура</p>
+        <p style={{ width: 1000 }}>2. Выучить React JS/Next JS</p>
+        <p>
+          3. React JS (средний уровень), могу делать несложные сервера на node js express, так же умею рабоать с
+          вебсокетами. Из баз данных умею работать с postgres
+        </p>
         <p style={{ width: 1000 }}>
           4. В универе дополнительно изучал web-программирование и после оканчания в конце прошлого года начал работать
           fronted разработчиком
