9bd78dba43e0e010cdbc58ab57176fb565951bbf
Yulia Starchenko <imay@amake.ru>
2024-03-07 17:46:29 +0300

    homework1: typescript 1 part

---
+++ src/homeworks/ts1/1_base.ts
@@ -0,0 +1,92 @@
+/**
+ * Нужно превратить файл в ts и указать типы аргументов и типы возвращаемого значения
+ * */
+export const removePlus = (string: string): string => string.replace(/^\+/, '');
+
+export const addPlus = (string: string): string => `+${string}`;
+
+export const removeFirstZeros = (value: string): string => value.replace(/^(-)?[0]+(-?\d+.*)$/, '$1$2');
+
+export const getBeautifulNumber = (value?: string | number, separator = ' '): string | void =>
+  value?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, separator);
+
+export const round = (value: number, accuracy = 2): number => {
+  const d = 10 ** accuracy;
+  return Math.round(value * d) / d;
+};
+
+type TransformFromCssResult = {
+  x: number;
+  y: number;
+};
+
+const transformRegexp =
+  /(matrix\(-?\d+(\.\d+)?, -?\d+(\.\d+)?, -?\d+(\.\d+)?, -?\d+(\.\d+)?, )(-?\d+(\.\d+)?), (-?\d+(\.\d+)?)\)/;
+
+export const getTransformFromCss = (transformCssString: string): TransformFromCssResult => {
+  const data: RegExpMatchArray = transformCssString.match(transformRegexp);
+  if (!data) return { x: 0, y: 0 };
+  return {
+    x: parseInt(data[6], 10),
+    y: parseInt(data[8], 10),
+  };
+};
+
+type ColorInRGB = [number, number, number];
+
+export const getColorContrastValue = ([red, green, blue]: ColorInRGB): number =>
+  // http://www.w3.org/TR/AERT#color-contrast
+  Math.round((red * 299 + green * 587 + blue * 114) / 1000);
+
+type ContrastType = 'black' | 'white';
+
+export const getContrastType = (contrastValue: number): ContrastType => (contrastValue > 125 ? 'black' : 'white');
+
+export const shortColorRegExp = /^#[0-9a-f]{3}$/i;
+export const longColorRegExp = /^#[0-9a-f]{6}$/i;
+
+type HEXColor = `#${string}`;
+
+export const checkColor = (color: HEXColor): void | never => {
+  if (!longColorRegExp.test(color) && !shortColorRegExp.test(color)) throw new Error(`invalid hex color: ${color}`);
+};
+
+checkColor('#000yyy');
+
+export const hex2rgb = (color: HEXColor): ColorInRGB | never => {
+  checkColor(color);
+  if (shortColorRegExp.test(color)) {
+    const red = parseInt(color.substring(1, 2), 16);
+    const green = parseInt(color.substring(2, 3), 16);
+    const blue = parseInt(color.substring(3, 4), 16);
+    return [red, green, blue];
+  }
+  const red = parseInt(color.substring(1, 3), 16);
+  const green = parseInt(color.substring(3, 5), 16);
+  const blue = parseInt(color.substring(5, 8), 16);
+  return [red, green, blue];
+};
+
+type NumberedValue<T> = { value: T; number: number };
+
+export const getNumberedArray = <T = unknown>(arr: T[]): NumberedValue<T>[] =>
+  arr.map((value: T, number: number): NumberedValue<T> => ({ value, number }));
+
+export const toStringArray = <T = unknown>(arr: NumberedValue<T>[]): string[] =>
+  arr.map(({ value, number }) => `${value}_${number}`);
+
+interface Customer {
+  id: number;
+  name: string;
+  age: number;
+  isSubscribed: boolean;
+}
+
+type transformCustomersResult = { [key: number]: Omit<Customer, 'id'> };
+
+export const transformCustomers = (customers: Customer[]): transformCustomersResult => {
+  return customers.reduce((acc: transformCustomersResult, customer: Customer) => {
+    acc[customer.id] = { name: customer.name, age: customer.age, isSubscribed: customer.isSubscribed };
+    return acc;
+  }, {});
+};
--- src/homeworks/ts1/3_write.ts
+++ src/homeworks/ts1/3_write.ts
@@ -1,3 +1,5 @@
+import { faker } from '@faker-js/faker';
+
 /**
  * Функции написанные здесь пригодятся на последующих уроках
  * С помощью этих функций мы будем добавлять элементы в список для проверки динамической загрузки
@@ -11,7 +13,15 @@
  * - id (строка)
  * - name (строка)
  * - photo (строка, необязательно)
- *
+ */
+
+interface Category {
+  id: string;
+  name: string;
+  photo?: string;
+}
+
+/**
  * Продукт (Product) содержит
  * - id (строка)
  * - name (строка)
@@ -21,9 +31,35 @@
  * - oldPrice (число, необязательно)
  * - price (число)
  * - category (Категория)
- *
+ */
+
+interface Product {
+  id: string;
+  name: string;
+  photo: string;
+  desc?: string;
+  createdAt: string;
+  oldPrice?: number;
+  price: number;
+  category: Category;
+}
+
+/**
  * Операция (Operation) может быть либо тратой (Cost), либо доходом (Profit)
- *
+ */
+
+type Operation = Cost | Profit;
+
+interface OperationBase {
+  id: string;
+  name: string;
+  desc: string;
+  createdAt: string;
+  amount: number;
+  category: Category;
+}
+
+/**
  * Трата (Cost) содержит
  * - id (строка)
  * - name (строка)
@@ -32,7 +68,13 @@
  * - amount (число)
  * - category (Категория)
  * - type ('Cost')
- *
+ */
+
+interface Cost extends OperationBase {
+  type: 'Cost';
+}
+
+/**
  * Доход (Profit) содержит
  * - id (строка)
  * - name (строка)
@@ -41,16 +83,60 @@
  * - amount (число)
  * - category (Категория)
  * - type ('Profit')
- * */
+ */
+
+interface Profit extends OperationBase {
+  type: 'Profit';
+}
 
 /**
  * Создает случайный продукт (Product).
  * Принимает дату создания (строка)
  * */
-// export const createRandomProduct = (createdAt: string) => {};
+
+const getImage = (): string => faker.image.urlLoremFlickr({ category: 'fashion' });
+
+const getCategory = (): Category => {
+  const categoryName = faker.helpers.arrayElement(['Dresses', 'Skirts', 'Accessories', 'Shoes', 'Bags']);
+  return {
+    id: faker.string.uuid(),
+    name: categoryName,
+    ...(faker.datatype.boolean() ? { photo: getImage() } : {}),
+  };
+};
+
+export const createRandomProduct = (createdAt: string): Product => {
+  const category = getCategory();
+  const oldPrice = +faker.commerce.price({ min: 980, max: 8900, dec: 0 });
+
+  return {
+    id: faker.string.uuid(),
+    name: `${faker.commerce.productAdjective()} ${faker.commerce.productMaterial()} ${category.name}`,
+    photo: getImage(),
+    ...(faker.datatype.boolean() ? { description: faker.commerce.productDescription() } : {}),
+    createdAt,
+    ...(faker.datatype.boolean() ? { oldPrice } : {}),
+    price: +faker.commerce.price({
+      min: oldPrice + 100 || 980,
+      max: oldPrice ? oldPrice + oldPrice * 0.5 : 8900,
+      dec: 0,
+    }),
+    category,
+  };
+};
 
 /**
  * Создает случайную операцию (Operation).
  * Принимает дату создания (строка)
  * */
-// export const createRandomOperation = (createdAt: string) => {};
+export const createRandomOperation = (createdAt: string): Operation => {
+  return {
+    id: faker.string.uuid(),
+    name: faker.finance.transactionType(),
+    desc: faker.finance.transactionDescription(),
+    createdAt,
+    amount: +faker.commerce.price({ min: 980, max: 8900, dec: 0 }),
+    category: getCategory(),
+    type: faker.helpers.arrayElement(['Cost', 'Profit']),
+  };
+};
